d3fdd6205a509ee63e38e693bbda90a3b5b302a9
upravaD <daktah@icloud.com>
2022-10-21 17:50:15 +0400

    add JUnit

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class HippodromeTest {
+    @Test
+    public void hippodromeTest(){
+
+    }
+
+    @Test
+    public void getHorsesTest(){
+
+    }
+
+    @Test
+    public void moveTest(){
+
+    }
+
+    @Test
+    public void getWinnerTest(){
+
+    }
+}
---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class HorseTest {
+    @Test
+    public void horseTest(){
+
+    }
+
+    @Test
+    public void getNameTest(){
+
+    }
+
+    @Test
+    public void getSpeedTest(){
+
+    }
+
+    @Test
+    public void getDistanceTest(){
+
+    }
+
+    @Test
+    public void moveTest(){
+
+    }
+}
